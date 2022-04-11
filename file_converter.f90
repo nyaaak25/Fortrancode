3a280b211da5f2fdd25ef2ef5b93a7498af0e868
nyaaak25 <kazama.akira.s2@dc.tohoku.ac.jp>
2022-04-11 17:56:11 +0900

    .k file make

--- file_converter.f90
+++ file_converter.f90
@@ -13,23 +13,25 @@ program Converter
     implicit none  ! おまじないで絶対に必要なもの
 
     ! parameter指定
-    character (LEN=1000) :: Openfile01,OPFIL0,Openfile02 ! 基本最初に型指定。長さの限界が100文字
-    INTEGER :: i, line_counter ! ALLOCATABLEはあとで確保してあげようとしている。確保しないと使えない。
-    double precision,allocatable::WN(:),Kw(:)
+    character (LEN=1000) :: Openfile01,Openfile02,OPFIL0,OPFIL1 ! 基本最初に型指定。長さの限界が100文字
+    INTEGER :: i, line_counter1, line_counter2 ! ALLOCATABLEはあとで確保してあげようとしている。確保しないと使えない。
+    double precision,allocatable::WN(:),Kw(:),Hight(:),Temp(:),press(:)
 
+
+    ! .v file makeing
     ! Input file
     Openfile01 = '/Users/nyonn/Desktop/pythoncode/4545-5556_0.01step_cutoff_120.txt'
     OPEN(1, FILE = Openfile01,STATUS='old')  ! 最初にメモリ確保する必要があるから、最初に配列数を知る必要がある
-    line_counter = 0
+    line_counter1 = 0
     do
         READ(1,*,end = 999)  ! ただのシグナルだけど、flagのような役割でREADでここまで来たら飛ぶような役割
-        line_counter = line_counter+1  ! 列をわかるようにしている
+        line_counter1 = line_counter1+1  ! 列をわかるようにしている
     enddo
     999 continue
     rewind(1)
-    allocate(WN(line_counter),Kw(line_counter))  ! メモリの確保ができたら、そのファイルを読み込む
+    allocate(WN(line_counter1),Kw(line_counter1))  ! メモリの確保ができたら、そのファイルを読み込む
 
-    DO i=1,line_counter
+    DO i=1,line_counter1
         READ(1,*) WN(i),Kw(i)
     ENDDO
 
@@ -41,8 +43,33 @@ program Converter
     OPFIL0 = 'vfile/LUtable.v'
     OPEN (3,FILE=OPFIL0,FORM='UNFORMATTED',STATUS='replace')
     WRITE(3) 1, 1
-    WRITE(3) 1, line_counter
+    WRITE(3) 1, line_counter1
     WRITE(3) WN
     CLOSE(3)
+
+    ! Input file
+    Openfile02 = '/Users/nyonn/Desktop/pythoncode/pre_temp_profile.txt'
+    OPEN(2, FILE = Openfile02,STATUS='old')  ! 最初にメモリ確保する必要があるから、最初に配列数を知る必要がある
+    line_counter2 = 0
+    do
+        READ(2,*,end = 99)  ! ただのシグナルだけど、flagのような役割でREADでここまで来たら飛ぶような役割
+        line_counter2 = line_counter2+1  ! 列をわかるようにしている
+    enddo
+    99 continue
+    rewind(2)
+    allocate(Hight(line_counter2),Temp(line_counter2),press(line_counter2))  ! メモリの確保ができたら、そのファイルを読み込む
+
+    DO i=1,line_counter2
+        READ(2,*) Hight(i),Temp(i),press(i)
+    ENDDO
+
+    CLOSE(2)
+
+    ! .k file making
+    OPFIL1 = 'kfile/LUtable.k'
+    OPEN (4,FILE=OPFIL1,FORM='UNFORMATTED',STATUS='replace')
+    WRITE(4) line_counter2
+    WRITE(4) Hight, Temp, press
+    CLOSE(4)
     
 end program Converter
